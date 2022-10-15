0077cf4a03e3e11e3a7d9d503cdb0902a290d06d
Kaio <kaiocostadasilva@outlook.com>
2022-10-15 17:04:26 -0400

    Create of a simple initial layout base in prototype.

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,57 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter: First Project'),
+          title: const Text('Tasks'),
         ),
-        body: Container(
-          color: Colors.white38,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Task('Aprender Flutter'),
+            Task('Pagar a energia'),
+            Task('Limpar casa'),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: (){},),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String name;
+
+  const Task(this.name, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.grey,
+                  width: 100,
+                ),
+                Text(this.name, style: TextStyle(fontSize: 24)),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_drop_up),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
