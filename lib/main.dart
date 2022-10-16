94d2a5b7c9307b4a0b823263d2d7e5a61e7591cd
Kaio <kaiocostadasilva@outlook.com>
2022-10-16 01:19:30 -0400

    Usage of ListView Widget to show multiple task cards on screen with scroll option; replacement of Stateless with Stateful to create the level counter that needs to store its value and present it on an updated screen; use of LinearProgressIndicator widget that allows monitoring the evolution of task levels in the proportion from 0 to 10; delete declared constants per IDE suggestion;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -17,57 +17,106 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Tasks'),
+          title: Text('Tasks'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
+        body: ListView(
+          children: [
             Task('Aprender Flutter'),
-            Task('Pagar a energia'),
+            Task('Pagar boleto da energia no 5 dia útil do mês'),
+            Task('Limpar casa'),
+            Task('Limpar casa'),
+            Task('Limpar casa'),
+            Task('Limpar casa'),
+            Task('Limpar casa'),
+            Task('Limpar casa'),
             Task('Limpar casa'),
           ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
+          child: Icon(Icons.add),
         ),
       ),
     );
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String name;
 
-  const Task(this.name, {Key? key}) : super(key: key);
+  Task(this.name, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
               children: [
                 Container(
-                  color: Colors.grey,
-                  width: 100,
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.grey,
+                        width: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(widget.name,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis))),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            level++;
+                          });
+                          print(level);
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                      ),
+                    ],
+                  ),
                 ),
-                Text(this.name, style: TextStyle(fontSize: 24)),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.arrow_drop_up),
-                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: level / 10,
+                        ),
+                      ),
+                      Text(
+                        'Nível: $level',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
